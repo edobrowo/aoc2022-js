32004dd26570f1a96a66578fca5e9039f682c180
glubbo <ethan.dobrowolski@gmail.com>
2023-07-13 23:09:43 -0400

    Day 01

---
+++ 01/maxCalories.js
@@ -0,0 +1,24 @@
+const { readFileSync } = require('fs');
+
+const inputPath = '01/adventofcode.com_2022_day_1_input.txt';
+
+const fileData = readFileSync(inputPath, 'utf8').trim();
+
+const elves = fileData.split('\n\n')
+    .map((caloriesList) => caloriesList.split('\n')
+    .map((item) => parseInt(item)));
+
+const sums = elves.map((caloriesList) => caloriesList.reduce((sum, cal) => sum + cal));
+
+const max = sums.reduce((max, cal) => max > cal ? max : cal);
+
+const top3 = sums.sort().slice(-3);
+// Or: const top3 = sums.sort((a, b) => (b - a) / Math.abs(b - a)).slice(0, 3)
+
+const sumTop3 = top3.reduce((sum, cal) => sum + cal);
+
+// Part 1
+console.log(max)
+
+// Part 2
+console.log()
